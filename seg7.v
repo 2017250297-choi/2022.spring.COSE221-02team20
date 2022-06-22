89b651cd39bfd8c75e5ed2bde489d680f6eecaf3
2017250297-choi <97ckdtn@gmail.com>
2022-06-22 20:32:50 +0900

    debug seg7.v
    debug

--- seg7.v
+++ seg7.v
@@ -4,16 +4,16 @@ output [6:0] display;
 reg[6:0]display;
 always @(data) begin
     case(data)
-        4'b00000 : display = 7'b1000000;
-        4'b00001 : display = 7'b1111001;
-        4'b00010 : display = 7'b0100100;
-        4'b00011 : display = 7'b0110000;
-        4'b00100 : display = 7'b0011001;
-        4'b00101 : display = 7'b0010010;
-        4'b00110 : display = 7'b0000010;
-        4'b00111 : display = 7'b1111000;
-        4'b01000 : display = 7'b0000000;
-        4'b01001 : display = 7'b0010000;
+        4'b0000 : display = 7'b1000000;
+        4'b0001 : display = 7'b1111001;
+        4'b0010 : display = 7'b0100100;
+        4'b0011 : display = 7'b0110000;
+        4'b0100 : display = 7'b0011001;
+        4'b0101 : display = 7'b0010010;
+        4'b0110 : display = 7'b0000010;
+        4'b0111 : display = 7'b1111000;
+        4'b1000 : display = 7'b0000000;
+        4'b1001 : display = 7'b0010000;
         default : display = 7'b0111111;
     endcase
 end
