31e04eb5a6113e46fe7b1dc063b9c83197e49cc1
2017250297-choi <97ckdtn@gmail.com>
2022-06-23 04:49:39 +0900

    double_seg7
    2digit 7segment display

---
+++ double_seg7.v
@@ -0,0 +1,17 @@
+module double_seg7 (
+    data_in,
+    seg10,
+    seg01
+);
+    input [6:0]data_in;
+    output[6:0] seg10 , seg01;
+    reg[3:0] ten, one;
+    seg7 s10 (ten,seg10);
+    seg7 s01 (one,seg01);
+    always @ (data_in)
+    begin
+        ten <=((data_in % 100)/10);
+        one <=(data_in % 10);
+    end
+
+endmodule
---
+++ tb_double_seg7.v
@@ -0,0 +1,11 @@
+`timescale 1ns/1ns
+module tb_double_seg7 ();
+    reg[6:0] data_in;
+    wire[3:0] out10, out01;
+    double_seg7 u0 (data_in,out10,out01);
+    initial
+    begin
+        data_in=0;
+        forever #5 data_in=data_in+1;
+    end
+endmodule
